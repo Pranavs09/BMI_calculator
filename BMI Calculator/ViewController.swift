22ffe0d0827fc744c16e944a7b047a447f1f6c35
Pranav Sharma <pranavs970@gmail.com>
2021-12-28 23:01:35 -0500

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Pranav Sharma on 2021-12-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
