75259892e908c52e9682c059021ea0e3fb1bb632
Pranav Sharma <pranavs970@gmail.com>
2021-12-30 17:40:43 -0500

    Initial Commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Pranav Sharma on 2021-12-28.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
